557dc06c9a73a3635ed2a4b7735460d2ad5493d0
Toeat <924198020@qq.com>
2018-09-13 00:01:06 +0800

    20180913

---
+++ 中亿雅联站/pc/js/index.js
@@ -0,0 +1,11 @@
+(function(){
+	//隐藏数字标题
+
+	if($(window).width() < 1350){
+		$('.solve .mid .box .num-tit').css('display','none');
+	}else{
+		$('.solve .mid .box .num-tit').css('display','block');
+	}
+	
+})();
+
