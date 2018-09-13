52d37c81a8ff414873970b38e8781c4ceae9ec02
特别能吃 <924198020@qq.com>
2018-09-13 17:32:05 +0800

    20180913

--- 中亿雅联站/pc/js/index.js
+++ 中亿雅联站/pc/js/index.js
@@ -1,10 +1,12 @@
 (function(){
 	//隐藏数字标题
-
 	if($(window).width() < 1350){
 		$('.solve .mid .box .num-tit').css('display','none');
+		$('.procedure .box .num-tit').css('display','none');
+		$('.product .box .num-tit').css('display','none');
 	}else{
 		$('.solve .mid .box .num-tit').css('display','block');
+		$('.product .box .num-tit').css('display','block');
 	}
 	
 })();
